589e8bd27fae4d674d7344dba1cac6b9d4ae3573
Daniel-RG <21danielrondong@gmail.com>
2023-01-13 20:02:59 +0100

    Iteration 3 done

--- routes/drones.js
+++ routes/drones.js
@@ -6,20 +6,26 @@ const Drone = require('../models/Drone.model')
 router.get('/drones', async (req, res, next) => {
   try {
     const allDrones = await Drone.find()
-    res.render('drones/list', { dronesArray: allDrones })
+    res.render('drones/list', { dronesArray: allDrones });
   } catch (error) {
     next(error)
   }
 });
 
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('drones/create-form');
 });
 
-router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+router.post('/drones/create', async (req, res, next) => {
+  const { name, propellers, maxSpeed } = req.body;
+  try {
+      const createdDrone = await Drone.create({ name, propellers, maxSpeed });
+      res.redirect('/drones');
+  }
+  catch (error) {
+      next(error)
+      res.redirect('/drones');
+  }
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
