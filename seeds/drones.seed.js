1528864999fb63e822a93e4d0e77c0b876d1b8f4
Daniel-RG <21danielrondong@gmail.com>
2023-01-13 17:57:37 +0100

    Iteration 1 done

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,33 @@
-// Iteration #1
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.model')
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+  const seedDB = async (drones) => {
+    try {
+
+        const dbConnection = await mongoose.connect(
+            "mongodb://localhost/lab-express-drones"
+        )
+
+        console.log('Connected to:', dbConnection.connections[0].name)
+
+        // Before adding any drone to the database, let's remove all existing ones
+        // await Drone.deleteMany();
+
+        const allDrones = await Drone.create(drones)
+        console.log("all the drones", allDrones);
+
+    } catch (error) {
+        console.log('An error occured')
+    } finally {
+        await mongoose.connection.close()
+    }
+}
+
+
+seedDB(drones)
